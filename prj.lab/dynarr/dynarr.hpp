144c3b301804eee8409be72be0dafaca9de720f9
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-02-08 22:05:16 +0300

    Update dynarr.hpp

--- prj.lab/dynarr/dynarr.hpp
+++ prj.lab/dynarr/dynarr.hpp
@@ -1,55 +1,27 @@
-// 2023 by Polevoi Dmitry under Unlicense
-
-#pragma once
-#ifndef DYNARR_DYNARR_HPP_20231203
-#define DYNARR_DYNARR_HPP_20231203
+#ifndef DYNARR_MISIS2023F_23_32
+#define DYNARR_MISIS2023F_23_32
 
 #include <cstddef>
-#include <sstream>
-#include <stdexcept>
-#include <iostream>
-
 
 class DynArr {
 public:
-  //конструктор по умолчанию
-  DynArr() {
-    size_ = 0;
-    capacity_ = 0;
-    data_ = NULL;  
-  }
-  //конструктор копии
-  DynArr(const DynArr& a) {
-    size_ = a.Size();
-    capacity_ = size_;
-    data_ = NULL;
-    if (size_ != 0) {
-      data_ = new float[size_];
-    }
-    for (int i = 0; i < size_; i++) {
-      data_[i] = a.data_[i];
-    }
-  }
-  //конструктор по размеру
-  DynArr(const std::ptrdiff_t& size);
+  DynArr() { capacity_ = 10; size_ = 0; mass_ = new float[10]; };
+  DynArr(const DynArr& rhs);
+  DynArr(const std::ptrdiff_t size);
+  ~DynArr();
 
-  ~DynArr() = default;
-
-  float& operator[](const std::ptrdiff_t index);
-
-  const float& operator[](const std::ptrdiff_t index) const;
-
-  [[nodiscard]] DynArr& operator=(const DynArr& rhs);
-
-  std::ptrdiff_t Size() const noexcept;
+  DynArr& operator= (const DynArr& rhs);
   void Resize(const std::ptrdiff_t size);
 
-private:
-  std::ptrdiff_t size_ = 0; //!< число элементов в массиве
-  std::ptrdiff_t capacity_ = 0; //вместимость массива
-  float* data_ = NULL;          //!< элементы массива
-};
+  float& operator[](const std::ptrdiff_t ind);
+  const float& operator[](const std::ptrdiff_t ind) const;
 
-std::ostream& operator<<(std::ostream& ostrm, DynArr& a) ;
+  std::ptrdiff_t Size() const { return size_; }
+  std::ptrdiff_t Capacity() const { return capacity_; }
+private:
+  std::ptrdiff_t size_ = 0;
+  std::ptrdiff_t capacity_ = 10;
+  float* mass_;
+};
 
 #endif
