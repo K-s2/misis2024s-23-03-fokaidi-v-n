2d4ba872c59992edd57b97eb2835a8ab36733dff
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-02-08 22:52:39 +0300

    Update stackarr.cpp

--- prj.lab/stackarr/stackarr.cpp
+++ prj.lab/stackarr/stackarr.cpp
@@ -1,1 +1,36 @@
+#include "StackArr.hpp"
+
+StackArr::StackArr(int size) {
+  capacity = size;
+  top = -1;
+  data_ = new Complex[size];
+}
+
+StackArr::~StackArr() {
+  delete[] data_;
+}
+
+void StackArr::push(Complex element) {
+  if (isFull()) {
+    std::cout << "Stack is full. Cannot push element." << std::endl;
+    return;
+  }
+  data_[++top] = element;
+}
+
+Complex StackArr::pop() {
+  if (isEmpty()) {
+    std::cout << "Stack is empty. Cannot pop element." << std::endl;
+    return Complex(); // Возвращаем пустой объект Complex
+  }
+  return data_[top--];
+}
+
+bool StackArr::isEmpty() {
+  return top == -1;
+}
+
+bool StackArr::isFull() {
+  return top == capacity - 1;
+}
 
