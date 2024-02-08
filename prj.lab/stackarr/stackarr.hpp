2edf98d8def621420060453dce98b877deb454e2
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-02-08 23:05:40 +0300

    Update stackarr.hpp

--- prj.lab/stackarr/stackarr.hpp
+++ prj.lab/stackarr/stackarr.hpp
@@ -1,1 +1,28 @@
+#ifndef STACKARR_HPP
+#define STACKARR_HPP
 
+#include <cstddef>
+#include <stdexcept>
+#include <complex/complex.hpp>
+
+class StackArr {
+private:
+  std::ptrdiff_t capacity;
+  std::ptrdiff_t size;
+  Complex* data_;
+
+public:
+  StackArr(std::ptrdiff_t size = 10);
+  StackArr(const StackArr& other);
+  ~StackArr();
+
+  void push(Complex element);
+  Complex pop();
+  Complex top();
+  bool isEmpty();
+  bool isFull();
+
+  StackArr& operator=(const StackArr& other);
+};
+
+#endif
