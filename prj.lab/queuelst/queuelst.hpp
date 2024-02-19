4f432ce2f0e2d0d03120edf155b9a28b19649ab7
viktya1top <endrviking@gmail.com>
2024-02-19 12:28:12 +0300

    ya ustal

--- prj.lab/queuelst/queuelst.hpp
+++ prj.lab/queuelst/queuelst.hpp
@@ -1,1 +1,15 @@
+class QueueLst{
+public:
+	QueueLst() = default;
 
+	QueueLst(const QueueLst&) = default;
+	~QueueLst() = default;
+private:
+  struct Node {
+    Node(Complex v_, Node* n) { v = v_; next = n; }
+    Complex v;
+    Node* next = nullptr;
+  };
+  Node* head_ = nullptr;
+  Node* tail_ = nullptr;
+}
--- prj.lab/stacklst/stacklst.cpp
+++ prj.lab/stacklst/stacklst.cpp
@@ -8,6 +8,19 @@ StackLst::StackLst(const StackLst& w) {
 		head_ = new Node(current->v, nullptr); // Создаем новый узел с данными из первого узла во входном стеке
 		Node* newCurrent = head_;
 		Node* oldCurrent = current;
+		int size_w = 1; // Учитываем первый узел
+
+		while (oldCurrent->next) {
+			oldCurrent = oldCurrent->next;
+			newCurrent->next = new Node(oldCurrent->v, nullptr); // Создаем новый узел для каждого узла во входном стеке
+			newCurrent = newCurrent->next;
+			size_w++;
+		}
+
+		Node* current = head_;
+		head_ = new Node(current->v, nullptr); // Создаем новый узел с данными из первого узла во входном стеке
+		Node* newCurrent = head_;
+		Node* oldCurrent = current;
 		int size_ = 1; // Учитываем первый узел
 
 		while (oldCurrent->next) {
@@ -16,8 +29,7 @@ StackLst::StackLst(const StackLst& w) {
 			newCurrent = newCurrent->next;
 			size_++;
 		}
-
-		// В size_ теперь хранится количество узлов в скопированном стеке
+		
 	}
 }
 
