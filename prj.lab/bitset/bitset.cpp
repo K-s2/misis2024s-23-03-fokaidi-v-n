534614cd5e574ea3d67bbd2f1fbb5eb1021f20cf
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-03-24 15:59:20 +0300

    Update bitset.cpp

--- prj.lab/bitset/bitset.cpp
+++ prj.lab/bitset/bitset.cpp
@@ -1,1 +1,18 @@
 #include <bitset/bitset.hpp>
+
+
+void BitSet::Fill(const bool val) noexcept {
+  if (val) {
+    for (int i = 0; i < bits_.size(); i++) {
+      bits_[i] = 4294967295;
+      size_ += 32;
+    }
+  }
+  else {
+    for (int i = 0; i < bits_.size(); i++) {
+      bits_[i] = 0;
+      size_ = 0;
+    }
+  }
+}
+
