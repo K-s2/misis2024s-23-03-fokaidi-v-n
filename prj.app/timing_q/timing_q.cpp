2bee4a456902d95dc9af2526b6b6f786f538e182
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-04-24 14:51:49 +0300

    Update timing_q.cpp

--- prj.app/timing_q/timing_q.cpp
+++ prj.app/timing_q/timing_q.cpp
@@ -1,3 +1,23 @@
-int main(){
+#include <chrono>
+#include <iostream>
+#include <queuelstt/queuelstt.hpp>
+
+int main()
+{
+  QueueLstT<int> q;
+
+  auto start = std::chrono::steady_clock::now(); // Здесь начинается измерение времени
+
+  // Добавление 10000 элементов в очередь
+  for (int i = 0; i < 1000000; ++i) {
+    q.Push(i);
+  }
+
+  auto end = std::chrono::steady_clock::now();   // Здесь заканчивается измерение времени
+
+  std::chrono::duration<double, std::milli> elapsed = end - start;
+
+  std::cout << "Pushing 1000000 elements took " << elapsed.count() << "ms.";
+
   return 0;
 }
