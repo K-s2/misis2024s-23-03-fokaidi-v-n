d34a77550fa12f46fd5c6d6d8f0dbc09a9201d90
K-s2 <81283622+K-s2@users.noreply.github.com>
2024-05-22 22:08:13 +0300

    Update timing_q.cpp

--- prj.app/timing_q/timing_q.cpp
+++ prj.app/timing_q/timing_q.cpp
@@ -1,7 +1,9 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <queuelstt/queuelstt.hpp>
+#include <cstdio>
 #include <queuearrt/queuearrt.hpp>
 
 int main()
@@ -14,7 +16,7 @@ int main()
   }
   auto end = std::chrono::steady_clock::now();   // Здесь заканчивается измерение времени
   std::chrono::duration<double, std::milli> elapsed = end - start;
-  std::cout << "Pushing 1, 2, 3, ....1000000 in queuelst elements took " << elapsed.count() << "ms."<<std::endl;
+  std::cout << "Pushing 1, 2, 3, ....1000000 in queuelst elements took " << elapsed.count() << "ms." << std::endl;
 
 
   QueueLstT<std::string> qs;
@@ -72,5 +74,69 @@ int main()
   std::chrono::duration<double, std::milli> ellapsed5 = end5 - start5;
   std::cout << "Pushing 1.1, 3.1, 6.1, ....500000500000.1 in queuearr elements took" << ellapsed5.count() << "ms." << std::endl;
 
+  //...
+  std::ofstream dataFile1, dataFile2;
+  dataFile1.open("timingData1.txt");
+  dataFile2.open("timingData2.txt");
+
+  // Данные для QueueLstT
+  dataFile1 << "DataType Time"<<std::endl;
+  dataFile1 << "Int " << elapsed.count() << std::endl;
+  dataFile1 << "String " << elapsed1.count() << std::endl;
+  dataFile1 << "Float " << ellapsed2.count() << std::endl;
+
+  // Данные для QueueArrT
+  dataFile2 << "DataType Time" << std::endl;
+  dataFile2 << "Int " << ellapsed3.count() << std::endl;
+  dataFile2 << "String " << elapsed4.count() << std::endl;
+  dataFile2 << "Float " << ellapsed5.count() << std::endl;
+
+  dataFile1.close();
+  dataFile2.close();
+
+  // Создайте файлы .gp с вашими инструкциями для gnuplot
+  std::ofstream gpFile1, gpFile2;
+  gpFile1.open("gnuplotInstructions1.gp");
+  gpFile2.open("gnuplotInstructions2.gp");
+
+  gpFile1 << "set terminal png"<<std::endl;
+  gpFile1<<"set title 'Data Pushing Timing" << std::endl;
+  gpFile1 << "set xlabel 'Data Type'"<<std::endl;
+  gpFile1 << "set ylabel 'Time (ms)'" << std::endl; 
+  gpFile1 << "set style data histogram" << std::endl;
+  gpFile1 << "set style histogram cluster gap 1" << std::endl;
+  gpFile1 << "set style fill solid" << std::endl;
+  gpFile1<< "set boxwidth 1" << std::endl;
+  gpFile1 << "set grid ytics" << std::endl;
+  gpFile1 << "set output 'QueueLstTPlot.png'" << std::endl;
+  gpFile1 << "plot  'timingData1.txt' using 2:xtic(1) title columnheader" << std::endl;
+  gpFile2 << "set terminal png" << std::endl; 
+  gpFile2 << "set title 'Data Pushing Timing" << std::endl;
+  gpFile2 << "set xlabel 'Data Type'" << std::endl;
+  gpFile2 << "set ylabel 'Time (ms)'" << std::endl;
+  gpFile2<<"set style data histogram" << std::endl;
+  gpFile2 << "set style histogram cluster gap 1" << std::endl;
+  gpFile2 << "set style fill solid" << std::endl;
+  gpFile2 <<"set boxwidth 1" << std::endl;
+  gpFile2 << "set grid ytics" << std::endl;
+  gpFile2 << "set output 'QueueArrTPlot.png'" << std::endl;
+  gpFile2 << "plot 'timingData2.txt' using 2:xtic(1) title columnheader"<<std::endl;
+  //std::string settings = "set terminal pngnset title 'Data Pushing Timing'n"
+  //  "set xlabel 'Data Type'nset ylabel 'Time (ms)'nset style data histogramn"
+  //  "set style histogram cluster gap 1nset style fill solidnset boxwidth 1n"
+  //  "set grid yticsnplot '";
+
+
+
+
+  gpFile1.close();
+  gpFile2.close();
+
+
+  // Запустите gnuplot с вашими файлами инструкций
+  system("gnuplot gnuplotInstructions1.gp");
+  system("gnuplot gnuplotInstructions2.gp");
+
   return 0;
+
 }
